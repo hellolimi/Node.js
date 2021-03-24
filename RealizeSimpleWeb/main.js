b2b0ca1ee58590073ef20e92086228400f011f86
hellolimi <hellolaimmn@gmail.com>
2021-03-24 15:53:09 +0900

    improve codes

--- RealizeSimpleWeb/main.js
+++ RealizeSimpleWeb/main.js
@@ -53,7 +53,11 @@ var app = http.createServer(function(request,response){
           `;
           button = `
           <a href="/create">Creat a list</a>
-          <a href="/update/?id=${title}">Update this list</a>
+          <a href="/update?id=${title}">Update this list</a>
+          <form action="/delete" name="delete" method="post">
+            <input type="hidden" value="${title}" name="id">
+            <button type="submit">Delete this list</button>
+          </form>
           `;
         }
         fs.readdir('./data', (err, fileList) => {
@@ -91,6 +95,55 @@ var app = http.createServer(function(request,response){
 
         fs.writeFile(`data/${title}`, description, 'utf-8', ()=> {
           response.writeHead(302, {Location:`/?id=${title}`});
+          response.end();
+        });
+      });
+    }else if(pathname == '/update'){
+      fs.readFile(`./data/${title}`, 'utf-8', (err, data) => {
+          description = `
+          <br><br>
+          <form action="http://localhost:3000/update_process" method="post" name="newList">
+            <input type="hidden" value="${title}" name="id">
+            <label for="title">Title</label><br>
+            <input type="text" name="title" id="title" value="${title}"><br><br>
+            <label for="description">Description</label><br>
+            <textarea name="description">${data}</textarea><br><br>
+            <button type="submit">Update</button>
+          </form>
+        `;
+        button = '';
+        fs.readdir('./data', (err, fileList) => {
+          response.writeHead(200);
+          response.end(templateHTML(title, templateList(fileList), description, button));
+        });
+      });
+    }else if(pathname == '/update_process'){
+      var body = '';
+      request.on('data', (data) => {
+        body += data;
+      });
+      request.on('end', () => {
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`./data/${id}`, `./data/${title}`, (err) => {
+          fs.writeFile(`data/${title}`, description, 'utf-8', ()=> {
+            response.writeHead(302, {Location:`/?id=${title}`});
+            response.end();
+          });
+        });
+      });
+    }else if(pathname == '/delete'){
+      var body = '';
+      request.on('data', (data) => {
+        body += data;
+      });
+      request.on('end', () => {
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, (err) => {
+          response.writeHead(302, {Location:`/`});
           response.end();
         });
       });
